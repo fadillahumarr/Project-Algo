b4d097782d6df273b0513c075dcca9ddc644d9eb
unknown <itsennuu@gmail.com>
2023-06-06 14:13:01 +0800

    new updated

--- keuangan.c
+++ keuangan.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> //digunakan dalam stdlib.h memiliki fungsi atof
+#include <stdlib.h>
 
-#define MAKS 50 // digunakan untuk membuat konstanta/macro
+#define MAKS 50 // deklarasi konstanta
 
-/*Penggunaan tipe data struct dilakukan agar dapat menyimpan banyak data
-    dengan tipe data berbeda*/
+// deklarasi tipe data struct
 struct data_tanggal
 {
     int tanggal;
@@ -17,20 +16,21 @@ struct Keuangan
     char kategori[MAKS];
     char jumlah[MAKS];
     char keterangan[MAKS];
-    struct data_tanggal tanggal_transaksi; // digunakan struct dalam struct sehingga memungkinkan mengakses anggota dari struct data_tanggal
+    struct data_tanggal tanggal_transaksi;
 };
 
-float saldoUangSekarang(int jumlah_transaksi, struct Keuangan Catat[100]) // fungsi yang akan menghitung saldo uang sekarang
+// fungsi untuk menghitung saldo uang sekarang
+float saldoUangSekarang(int jumlah_transaksi, struct Keuangan Catat[100])
 {
-    float saldo = 0.0; // deklarasi variabel yang akan menampung nilai return dgn inisialisasi awal  0
+    float saldo = 0.0;
     for (int i = 0; i < jumlah_transaksi; i++)
     {
-        float jumlah = atof(Catat[i].jumlah);             // atof() disini gunakan untuk mengonversi string menjadi nilai floating-point (desimal) karena kan awalnya dideklarasi pakai char jumlah
-        if (strcmp(Catat[i].kategori, "Pendapatan") == 0) // strcmp itu untung membandingkan string. nah kalau misalnya string dalam catat[i].kategori sama dengan pendapatan maka nanti bakal mengembalikan nilai 0 kalau stringnya sama.
+        float jumlah = atof(Catat[i].jumlah);
+        if (strcmp(Catat[i].kategori, "Pendapatan") == 0)
         {
-            saldo += jumlah; // disini saldo akan menampung nilai dari saldo + jumlah
+            saldo += jumlah;
         }
-        else if (strcmp(Catat[i].kategori, "Pengeluaran") == 0) // ini kalau misalnya string dalam catat[i].kategori sama dengan pengeluaran maka nanti bakal mengembalikan nilai 0 kalau stringnya sama. dan memproses saldo = saldo +jumlah
+        else if (strcmp(Catat[i].kategori, "Pengeluaran") == 0)
         {
             saldo -= jumlah;
         }
@@ -38,10 +38,10 @@ float saldoUangSekarang(int jumlah_transaksi, struct Keuangan Catat[100]) // fun
     return saldo;
 }
 
-// ini mirip ji penjelasannya kayak yang fungsi sebelumnya
+// fungsi untuk menghitung total pendapatan
 float totalPendapatan(int jumlah_transaksi, struct Keuangan Catat[100])
 {
-    float total_pendapatan = 0.0; // variabel yang akan menampung total pendapatan
+    float total_pendapatan = 0.0;
     for (int i = 0; i < jumlah_transaksi; i++)
     {
         if (strcmp(Catat[i].kategori, "Pendapatan") == 0) //
@@ -51,7 +51,8 @@ float totalPendapatan(int jumlah_transaksi, struct Keuangan Catat[100])
     }
     return total_pendapatan;
 }
-// ini juga mirip ji penjelasannya kayak yang fungsi sebelumnya
+
+// fungsi untuk menghitung total pendapatan
 float totalPengeluaran(int jumlah_transaksi, struct Keuangan Catat[100])
 {
     float total_pengeluaran = 0.0;
@@ -67,10 +68,10 @@ float totalPengeluaran(int jumlah_transaksi, struct Keuangan Catat[100])
 
 int main()
 {
-    struct Keuangan Catat[100];        // dideklarasikan sebuah array yang bisa menampung 100 elemen yang bertipe struct Keuangan
-    int pilihan, jumlah_transaksi = 0; // deklarasi variabel pilihan yang nanti minta input dari user dan juga jumlah_transaksi yang sudah dianalisasi = 0
-    int inputKategori, saldo;          // deklarasi inputKategori sehingga memungkinkan memperoleh input dari user nantinya, deklarasi saldo untuk menampung stotal saldo yang dimiliki
-    while (1)                          // menggunakan while agar program terus berjalan dan menampilkan menu kepada pengguna untuk pilih opsi karena kalau tidak pakai while, maka program hanya akan selesai setelah satu instruksi dilakukan
+    struct Keuangan Catat[100];
+    int pilihan, jumlah_transaksi = 0;
+    int inputKategori, saldo;
+    while (1)
     {
         printf("\n-----PROJECT MANAJEMEN KEUANGAN-----\n");
         printf("\n1. Mencatat Pemasukan/Pengeluaran");
@@ -80,40 +81,37 @@ int main()
         printf("\n5. Saldo Uang Sekarang");
         printf("\n6. Berhenti mencatat transaksi");
         printf("\nMasukkan input:");
-        scanf("%d", &pilihan); // disini nanti minta input dari user, mau lakukan apa
-        if (pilihan == 1)      // kalau pilih 1, berarti nanti akan mencatat pemasukan/pengerluaran
+        scanf("%d", &pilihan);
+        if (pilihan == 1) // Mencatat pemasukan/pengeluaran
         {
             printf("\nMasukkan Tanggal(DD/MM/YYYY)\t: ");
             scanf("%d/%d/%d", &Catat[jumlah_transaksi].tanggal_transaksi.tanggal,
                   &Catat[jumlah_transaksi].tanggal_transaksi.bulan,
                   &Catat[jumlah_transaksi].tanggal_transaksi.tahun);
-            /* disini nanti akan memasukkan/menyimpan nilai ke dalam array 'CATAT' bertipe 'struct keuangan' dengan elemen sebanyak 'jumlah_transaksi'
-                dengan anggota 'tanggal_transaksi.tanggal', 'tanggal_transaksi.bulan' dan 'tanggal_transaksi.tahun'*/
-            getchar(); /*kemudian menggunakan getchar() agar apabila terdapat karakter new line (enter) yang tersisa dalam input buffer maka kita bisa mebaca dan menghapusnya sebelum input berikutnya sehingga tidak diabaikan dan tidak mempengaruhi pembacaan input selanjutnya.
-            Karena pada dasarnya fungsi getchar itu untuk membaca satu karakter dari input buffer. Buffer itu kek memori yang digunakan untuk menyimpan data sementara  */
+            getchar();
             printf("Kategori:");
             printf("\n1. Pendapatan");
             printf("\n2. Pengeluaran");
             printf("\nMasukkan kategori(1/2)\t\t: ");
             scanf("%d", &inputKategori);
             getchar();
-            if (inputKategori == 1) // kalau misalnya user pilih 1, maka nanti "Pendapatan" akan disalin kedalam Catat[jumlah_transaksi]. kategori dengan menggunakan strcpy
+            if (inputKategori == 1)
             {
                 strcpy(Catat[jumlah_transaksi].kategori, "Pendapatan");
             }
-            else if (inputKategori == 2) // kalau misalnya user pilih 1, maka nanti "Pengeluaran" akan disalin kedalam Catat[jumlah_transaksi]. kategori dengan menggunakan strcpy
+            else if (inputKategori == 2)
             {
                 strcpy(Catat[jumlah_transaksi].kategori, "Pengeluaran");
             }
-            printf("Masukkan Keterangan\t\t: ");
-            scanf("%[^\n]%*c", Catat[jumlah_transaksi].keterangan); // pakai %[^\n]%*c sebagai format untuk memasukkan string yang mengandung spasi atau karekter khusus lainnya selain newline dan juga setelah scanf tidak dibuthkan lagi getchar karena udah ada format %*c yang digunakan untuk abaikan newline
             printf("Jumlah(Rp)\t\t\t: ");
             scanf("%[^\n]%*c", Catat[jumlah_transaksi].jumlah);
+            printf("Masukkan Keterangan\t\t: ");
+            scanf("%[^\n]%*c", Catat[jumlah_transaksi].keterangan);
             jumlah_transaksi++;
         }
-        else if (pilihan == 2)
+        else if (pilihan == 2) // Menampilkan Riwayat Transaksi
         {
-            if (jumlah_transaksi == 0) // kalau nanti belum masukkan transaksi apapun, nanti bakal menampilkan ini
+            if (jumlah_transaksi == 0)
             {
                 printf(" ---------------------------------------------------------------- ");
                 printf("\n| Belum ada pencatatan transaksi pendapatan maupun pengeluaran |\n");
@@ -121,32 +119,31 @@ int main()
             }
             else
             {
-                for (int j = 0; j < jumlah_transaksi; j++) // ini untuk menampilkan semua catatan transaksi yang sudah di masukkan
+                for (int j = 0; j < jumlah_transaksi; j++)
                 {
                     printf("\nTransaksi %d", j + 1);
                     printf("\nTanggal\t\t: %d/%d/%d", Catat[j].tanggal_transaksi.tanggal,
                            Catat[j].tanggal_transaksi.bulan,
                            Catat[j].tanggal_transaksi.tahun);
                     printf("\nKategori\t: %s", Catat[j].kategori);
-                    printf("\nKeterangan\t: %s", Catat[j].keterangan);
                     printf("\nJumlah(Rp)\t: %s", Catat[j].jumlah);
+                    printf("\nKeterangan\t: %s", Catat[j].keterangan);
                     printf("\n-----------------------------\n");
                 }
             }
         }
-        else if (pilihan == 3)
+        else if (pilihan == 3) // Menghitung Total Pendapatan
         {
             printf(" ----------------------------- ");
-            printf("\n| Total Pendapatan = Rp %.2f |\n", totalPendapatan(jumlah_transaksi, Catat)); // ini akan menampung return dari fungsi yang dipanggil yaitu  totalPendapatan
+            printf("\n| Total Pendapatan = Rp %.2f |\n", totalPendapatan(jumlah_transaksi, Catat));
             printf(" ----------------------------- ");
         }
-        else if (pilihan == 4)
+        else if (pilihan == 4) // Menghitung Total Pengeluaran
         {
             printf(" ----------------------------- ");
-            printf("\n| Total Pengeluaran = Rp %.2f |\n", totalPengeluaran(jumlah_transaksi, Catat)); // ini akan menampung return dari fungsi yang dipanggil yaitu  totalPengeluaran
-            printf(" ----------------------------- ");
+            printf("\n| Total Pengeluaran = Rp %.2f |\n", totalPengeluaran(jumlah_transaksi, Catat));
         }
-        else if (pilihan == 5)
+        else if (pilihan == 5) // Menghitung Saldo Uang Sekarang
         {
             if (jumlah_transaksi == 0)
             {
@@ -157,15 +154,14 @@ int main()
             else
             {
                 printf(" ---------------------- ");
-                printf("\n| Saldo Uang Sekarang = Rp %.2f |\n", saldoUangSekarang(jumlah_transaksi, Catat)); // ini akan menampung return dari fungsi yang dipanggil yaitu  saldoUangSekarang
-                printf(" ---------------------- ");
+                printf("\n| Saldo Uang Sekarang = Rp %.2f |\n", saldoUangSekarang(jumlah_transaksi, Catat));
             }
         }
-        else if (pilihan == 6) // ini kalau ingin berhenti mencatat transaksi
+        else if (pilihan == 6) // Menghentikan pencatatan transaksi
         {
             break;
         }
-        else // ini kalau yang na input selain 1-6
+        else // Jika input selain 1-6
         {
             printf("Input Tidak Valid!!!");
             break;
